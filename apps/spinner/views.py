0a2c661229f44bcc5539ba056a052725d1445164
Piotr Zalewa <piotr@zalewa.info>
2010-11-01 09:48:04 +0000

    changed the time

--- apps/spinner/views.py
+++ apps/spinner/views.py
@@ -28,7 +28,7 @@ def start_test(req, thanks=False, template='spinner/index.html'):
     xhr_duration_set = ('1.0', '3.0')
     spinner_delay_set = {
             '1.0': (0.0, 0.2, 0.4),
-            '3.0': (0.0, 0.4, 1.0),
+            '3.0': (0.0, 0.2, 0.4),
             '5.0': (0.0, 0.4, 1.0)
             }
 
