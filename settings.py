7ba3ab8a3ceebf15c0cf55a5476cd9b4a33e30b5
Piotr Zalewa <piotr@zalewa.info>
2011-06-08 14:10:47 +0100

    gunicorn in settings

--- settings.py
+++ settings.py
@@ -98,7 +98,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'spinner'
+    'spinner',
+    'gunicorn'
 )
 
 
